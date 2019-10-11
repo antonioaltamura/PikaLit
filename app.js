cd314734125f36166740ad4c0a650f7f85ad0fbf
antonioaltamura <antonioaltamura7@gmail.com>
2019-10-11 12:08:34 +0100

    first

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-/**
- * Created by Antonio Altamura on 10/10/2019.
- */
+//import { html, LitElement } from 'https://unpkg.com/@polymer/lit-element@latest/lit-element.js?module';
+import { html, LitElement } from '@web/lit-element.js';
+
+export default class App extends LitElement {
+    constructor() {
+        super()
+    }
+    createRenderRoot() {
+        return this;
+    }
+    render() {
+        return html`
+        <div>
+            Hello Pika!
+        </div>
+        `;
+    }
+}
+
+customElements.define('my-app',App);
