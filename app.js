72aa8840a84480f561b984247059ff7286e843ba
antonioaltamura <antonioaltamura7@gmail.com>
2019-10-11 13:14:18 +0100

    first

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //import { html, LitElement } from 'https://unpkg.com/@polymer/lit-element@latest/lit-element.js?module';
 import { html, LitElement } from '@web/lit-element.js';
 
+
 export default class App extends LitElement {
     constructor() {
         super()
